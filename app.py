5b091a69dbfd6d7e034912a89d9784d8798051ea
Aviva <avivalipkowitz@gmail.com>
2014-07-09 16:59:07 -0700

    End of day July8, added bootstrap template, need to work on updating with my own styles

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask, request, session, render_template, g, redirect, url_for, flash
 import jinja2
+import model
 
 app = Flask(__name__)
+SECRET_KEY = "fish"
+app.config.from_object(__name__)
 
 
 @app.route('/')
@@ -9,8 +12,76 @@ def index():
 	return render_template('base.html')
 
 
-@app.route('/login')
-def
+@app.route('/login', methods = ['GET'])
+def show_login():
+	return render_template('login.html')
+
+@app.route('/login', methods = ['POST'])
+def process_login():
+	f = request.form
+
+	user_email = f.get('email')
+	user_password = f.get('password')
+
+	user_login = model.session.query(model.Doula).filter_by(email = user_email).filter_by(password = user_password).first()
+
+	print "*******************"
+	print "user_login is", user_login
+
+	if not user_login:
+		print "invalid login"
+	else:
+		session['email'] = user_email
+		flash("You succesfully logged in!")
+	print "###############"
+	print "session is", session
+
+	return redirect('/')
+
+@app.route('/logout')
+def logout():
+	## FIX THIS SO YOU CAN'T BREAK IT BY LOGGING OUT
+	## WHEN YOU HAVEN'T LOGGED IN!
+	if not session['email']:
+		flash("You have to log in before you log out")
+		return redirect('/')
+	else:
+		del session['email']
+	print "###############"
+	print "you succesfully logged out"
+	print "session is", session
+	return redirect('/')
+
+
+
+@app.route('/signup', methods = ['GET'])
+def process_signup():
+	return render_template('sign-up.html')
+
+
+@app.route('/signup', methods = ['POST'])
+def signup():
+	f = request.form
+
+	user_email = f.get('email')
+	user_password = f.get('password')
+
+	user_login = model.session.query(model.Doula).filter_by(email = user_email).filter_by(password = user_password).first()
+
+	print "*******************"
+	print "user_login is", user_login
+
+	if not user_login:
+		print "invalid login"
+	else:
+		session['email'] = user_email
+		flash("You succesfully logged in!")
+
+	return redirect('/')
+
+
+
+
 
 if __name__ == '__main__':
 	app.debug = True
--- model.py
+++ model.py
@@ -2,10 +2,14 @@
 import datetime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
-from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy import Column, Integer, String, DateTime, Text
 from sqlalchemy.orm import sessionmaker, scoped_session
 from sqlalchemy import ForeignKey
 from sqlalchemy.orm import relationship, backref
+
+#from ratings tutorial
+ENGINE = None
+Session = None
 
 # from ratings model.py
 engine = create_engine("sqlite:///doulatree.db", echo = False)
@@ -29,16 +33,21 @@ class Doula(Base):
 	firstname = Column(String(64), nullable = True)
 	lastname = Column(String(64), nullable = True)
 	practice = Column(String(64), nullable = True)
-	email = Column(String(64), nullable = True)
-	phone = Column(String(64), nullable = True)
-	website = Column(String(64), nullable = True)
-	price = Column(Integer, nullable = True)
-	background = Column(String(500), nullable = True)
-	services = Column(String(200), nullable = True)
+	phone = Column(String(20), nullable = True)
+	website = Column(String(100), nullable = True)
+	price_min = Column(Integer, nullable = True)
+	price_max = Column(Integer, nullable = True)
+	background = Column(Text, nullable = True)
+	services = Column(Text, nullable = True)
 	reviews = Column(String(200), nullable = True)
 	image = Column(String(64), nullable = True)
 	zipcode = Column(String(64), nullable = True)
 
+def main():
+    """In case we need this for something"""
+    pass
 
+if __name__ == "__main__":
+    main()
 
 
